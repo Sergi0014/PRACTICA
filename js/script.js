43c26b7406341097a52d1f31ba61cc4f37cf90a0
Sergi0024 <162249445+Sergi0014@users.noreply.github.com>
2024-07-13 16:16:42 -0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function showChart() {
+   
+    document.getElementById('cityPercentages').style.display = 'block';
+}
+
+function hideChart() {
+   
+    document.getElementById('cityPercentages').style.display = 'none'  
+}
